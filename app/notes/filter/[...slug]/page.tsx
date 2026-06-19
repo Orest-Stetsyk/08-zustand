f1e39d83aa7dc0604a86c1c85fd4e4d0713f1fe7
Orest-Stetsyk <orest.stetsik@gmail.com>
2026-06-19 15:22:07 +0300

    Meta tegs and Open Graph

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,11 +5,43 @@ import {
 } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
+import { Metadata } from "next"
+
 
 const perPage = 12;
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const currentSlug = slug[0] ?? 'all';
+  const isAllNotes = currentSlug.toLowerCase() === 'all';
+  const filterName = isAllNotes ? 'All' : currentSlug;
+  const title = `${filterName} notes | NoteHub`;
+  const description = isAllNotes
+    ? 'View all notes in the NoteHub application.'
+    : `View notes filtered by the ${filterName} category.`;
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `https://notehub.com/notes/filter/${slug.join('/')}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: `${filterName} notes in NoteHub`,
+        },
+      ],
+    },
+  };
+}
 
 const NotesPage = async ({ params }: Props) => {
   const { slug } = await params;
